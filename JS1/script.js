a0f069fbe8e0e7a6393e439137c4876e5c4cfc74
LuccaDC <80166338+LuccaDC@users.noreply.github.com>
2024-05-20 22:22:44 -0400

    Update Exercício JS1

--- JS1/script.js
+++ JS1/script.js
@@ -1,6 +1,6 @@
 function tabela(){
     for(let i=1; i<11; i++){
-        document.write("<table style='display:inline-table; margin: 10px; text-align: center;' border = 1> <thead> <tr> <th colspan='2'> Produtos de "+i+" </th> </tr> </thead> <tbody>");
+        document.write("<table border = 1> <thead> <tr> <th colspan='2'> Produtos de "+i+" </th> </tr> </thead> <tbody>");
         for(let j=1; j<11; j++){
             document.write("<tr> <td> "+i+"X"+j+" </td> <td> "+i*j+" </td> </tr>");
         }
