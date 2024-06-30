e48c8231490f34fbbf46282f1b9e89f65dcf8170
LuccaDC <80166338+LuccaDC@users.noreply.github.com>
2024-06-30 04:04:53 -0400

    Exercício JS4

---
+++ JS4/script.js
@@ -0,0 +1,12 @@
+window.onload = function(){ 
+    let raio = document.getElementById("raio");
+    let area = document.getElementById("area");
+    let circ = document.getElementById("circ");
+    const btn = document.getElementById("btn");
+
+    btn.onclick = function(){
+        r = raio.value;
+        area.value = (Math.PI * Math.pow(r,2)).toFixed(2);
+        circ.value = (2 * Math.PI * r).toFixed(2);
+    }; 
+};
